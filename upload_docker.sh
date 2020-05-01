539a4bba9378757867619eea093c5a328d4af39d
Ubuntu <ubuntu@ip-172-31-31-194.us-west-2.compute.internal>
2020-05-01 18:58:11 +0000

    log improvement, log file, and upload docker image

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,13 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=dantesaggin/development
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker tag development $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
